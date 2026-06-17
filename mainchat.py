5959fa6b419876178d675d84b3af845e3a10954e
fauzularifin99 <fauzularifin99@gmail.com>
2026-06-18 00:57:03 +0700

    Update mainchat.py

--- mainchat.py
+++ mainchat.py
@@ -648,7 +648,7 @@ elif st.session_state.halaman == 3:
         min-height: 40px;
     }
     iframe {
-        max-height: 330px;
+        max-height: 280px;
     }
     </style>
     """, unsafe_allow_html=True)
