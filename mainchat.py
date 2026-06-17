5e0bda2da7913dd4aaeea3aa9e1eeaccca7afe9e
fauzularifin99 <fauzularifin99@gmail.com>
2026-06-18 00:27:56 +0700

    Update mainchat.py

--- mainchat.py
+++ mainchat.py
@@ -43,6 +43,8 @@ def load_model():
 class DangerDetector:
     def __init__(self):
         self.alert_counter = 0
+        self.frame_count = 0
+        self.last_danger_detected = False
         self.model = load_model()
 
     def recv(self, frame):
@@ -50,23 +52,24 @@ class DangerDetector:
         import cv2
    
         img = frame.to_ndarray(format="bgr24")
-   
-        if self.model is None:
-            cv2.putText(img, "MODEL ERROR", (30, 60),
-                       cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
-   
-        results = self.model(img, conf=CONF_THRESHOLD, imgsz=640, verbose=False)[0]
-
-        danger_detected = False
-
-        for box in results.boxes:
-            cls_id = int(box.cls[0])
-            label = self.model.names[cls_id]
-
-            if label in DANGEROUS_CLASSES:
-                danger_detected = True
-                break
+        self.frame_count += 1
+        
+            if self.frame_count % 3 == 0:
+                results = self.model(img, conf=CONF_THRESHOLD, imgsz=320, verbose=False)[0]
+        
+                danger_detected = False
+        
+                for box in results.boxes:
+                    cls_id = int(box.cls[0])
+                    label = self.model.names[cls_id]
+        
+                    if label in DANGEROUS_CLASSES:
+                        danger_detected = True
+                        break
+        
+                self.last_danger_detected = danger_detected
+        
+            danger_detected = self.last_danger_detected
 
         if danger_detected:
             self.alert_counter = ALERT_HOLD_FRAMES
